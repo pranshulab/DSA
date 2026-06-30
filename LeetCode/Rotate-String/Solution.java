22e778ea1c273c829fd0ca6671055829d0312905
Pranshu katiyar <144892641+pranshulab@users.noreply.github.com>
2026-07-01 01:38:54 +0530

    Add solution for Rotate String

--- LeetCode/Rotate-String/Solution.java
+++ LeetCode/Rotate-String/Solution.java
@@ -1,9 +1,9 @@
 1class Solution {
 2    public boolean rotateString(String s, String goal) {
-3        String str = s + s;
-4
+3        if(s.length() != goal.length()) {return false;}
+4        String str = s+s;
 5        if(str.contains(goal)) {
-6            return true;  
+6            return true;
 7        }
 8        return false;
 9    }
